48570ee4848b46bae6ab60722a7fe969a9f55905
RedBoy5093 <redboy5093@gmail.com>
2022-06-26 16:22:14 +0430

    better version

--- index.py
+++ index.py
@@ -1,60 +1,81 @@
-# index.py app    
+# index.py app
 
-                                                 
 
-print('Hello ! welcome to Amir app please choose one of this :')                          
+print('Hello ! welcome to Amir app please choose one of this :')
 
-print('circle code number :1 , recteangle code number:2 , squre code number:3 triangle code number:4')    # code numbers       
+# code numbers
+print('circle code number :1 , rectangle code number:2 , square code number:3 triangle code number:4')
 
 code = input("enter your code :")                                 # code input
 
 code = int(code)                                  # code type
-   
 
-if code < 2 and code > 0 :                      # circle if code
-  square()
 
-elif code>1 and code<3 :                    # rectangle if code
+#--------------------------Functions----------------------------------#
+def circle():
+    radius = input("please give me your radius :")               # radius input
+
+    radius = int(radius)                        # radius type
+
+    pi = 3.14                                # pi
+
+    circle_area = (radius**2)*pi           # circle area formula
+
+    circle_perimeter = (2*pi)*radius           # circle environment formula
+
+    print("your circle area is :", circle_area)          # print of output area
+
+    # print of output environment
+    print('your circle perimeter is:', circle_perimeter)
+
+
+def rectangle():
 
     width = input('please give me your width :')       # width input
 
     width = int(width)                 # width type
 
-    the_length = input('please give me your your length :')    # the length input
+    # the length input
+    the_length = input('please give me your your length :')
 
     the_length = int(the_length)          # the length type
 
-    rectangle_area = (width*the_length)        # rectangle area formula 
+    rectangle_area = (width*the_length)        # rectangle area formula
 
-    rectangle_perimeter = (width + the_length)*2      # recteangle environment formula
+    # recteangle environment formula
+    rectangle_perimeter = (width + the_length)*2
 
-    if width>the_length :                     # if code of width and the length rectangle
+    if width > the_length:                     # if code of width and the length rectangle
 
         rectangle_area = False             # false area
 
         rectangle_perimeter = False           # false environment
 
-        print('is not true becuse width a more of the length')      # if print 
+        print('is not true becuse width a more of the length')      # if print
 
-    print("your rectangle area is:" , rectangle_area)         # print output of rectangle area
+    # print output of rectangle area
+    print("your rectangle area is:", rectangle_area)
 
-    print('your rectangle perimeter is:' , rectangle_perimeter)       #print output pf rectangle environment
+    # print output pf rectangle environment
+    print('your rectangle perimeter is:', rectangle_perimeter)
 
-elif code < 4 and code > 2 :   
-    
-    squre_side = input('please give me your squre side :')
 
-    squre_side = int(squre_side)
+def square():
 
-    squre_perimeter = (4 * squre_side)
+    square_side = input('please give me your square side :')
 
-    squre_area = (squre_side * squre_side)
+    square_side = int(square_side)
 
-    print('your squre area is :' , squre_area)
+    square_perimeter = (4 * square_side)
 
-    print('your squre oerimeter is :' , squre_perimeter)
+    square_area = (square_side * square_side)
 
-elif code < 6 and code > 3 :
+    print('your square area is :', square_area)
+
+    print('your square oerimeter is :', square_perimeter)
+
+
+def triangle():
 
     triangle_first_side = input("plese give me your first side :")
 
@@ -72,34 +93,35 @@ elif code < 6 and code > 3 :
 
     triangle_height = int(triangle_height)
 
-    triangle_primeter = (triangle_first_side + triangle_second_side + triangle_base)
+    triangle_primeter = (triangle_first_side +
+                         triangle_second_side + triangle_base)
 
     triangle_area = (triangle_height * triangle_base) / 2
 
-    print("your triangle area is :" , triangle_area)
+    print("your triangle area is :", triangle_area)
 
-    print("your triangle perimeter is :" , triangle_primeter)
-else :
-   print("code is false")   
-
-
-def square():
-    radius = input("please give me your radius :")               # radius input      
-
-    radius = int(radius)                        # radius type
-    
-    pi = 3.14                                # pi
-    
-    circle_area = (radius**2)*pi           # circle area formula
-    
-    circle_perimeter = (2*pi)*radius           # circle environment formula
-    
-    print("your circle area is :" , circle_area)          # print of output area
-    
-    print('your circle perimeter is:' , circle_perimeter)    # print of output environment
-
- 
+    print("your triangle perimeter is :", triangle_primeter)
+#--------------------------End of functions----------------------------------#
 
 
+if code == 1:                      # circle if code
+    circle()
 
-input("Press enter to exit ;)")
+
+elif code == 2:                    # rectangle if code
+    rectangle()
+
+
+elif code == 3:                   # square if code
+    square()
+
+
+elif code == 4:  # triangle if code
+    triangle()
+
+
+else:  # if non of above
+    print("code is false")
+
+
+input("Press enter to exit ;)")  # to prevent the cmd from closing
